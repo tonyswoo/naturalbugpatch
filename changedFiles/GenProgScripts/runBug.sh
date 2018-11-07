2748674697e8cdad996858fc2a6171c976d7ed50
tonyswoo <tswoo@ucdavis.edu>
2018-11-07 01:27:51 -0800

    Fix relative path to absolute

--- changedFiles/GenProgScripts/runBug.sh
+++ changedFiles/GenProgScripts/runBug.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-$SCRIPTS/runGenProgForBug.sh $1 $2 allHuman 100 D4JwithGP $4 $(( $4 + 1 )) false $JAVA7_HOME $JAVA9_HOME false a false a $3 $5 2>> errors/$1$2
+$SCRIPTS/runGenProgForBug.sh $1 $2 allHuman 100 D4JwithGP $4 $(( $4 + 1 )) false $JAVA7_HOME $JAVA9_HOME false a false a $3 $5 2>> $SCRIPTS/errors/$1$2
 $SCRIPTS/extractTars.sh ${1,,}$2"Buggy"
 
